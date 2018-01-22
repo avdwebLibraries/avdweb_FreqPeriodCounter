a6760f5b7acd8161e66f89717e949a194cbadf05
Albert van Dalen <albertfora@avdweb.nl>
2018-01-22 09:31:09 +0100

    Add files via upload

---
+++ examples/Frequency-counter-using-polling/Simple-example/Simple-example.ino
@@ -0,0 +1,13 @@
+#include <FreqPeriodCounter.h>
+  
+const byte counterPin = 3;
+FreqPeriodCounter counter(counterPin, micros);
+  
+void setup(void)
+{
+}
+  
+void loop(void)
+{ counter.poll();
+  float f = (float)1 / counter.period;
+}
