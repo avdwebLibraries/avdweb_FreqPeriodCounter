197a4a2e0cb719e9309f22be08c5ae4f69f706ab
Albert van Dalen <albertfora@avdweb.nl>
2018-01-22 09:34:09 +0100

    Add files via upload

---
+++ examples/Simple-interrupt-example/Simple-interrupt-example.ino
@@ -0,0 +1,18 @@
+#include <FreqPeriodCounter.h>
+  
+const byte counterPin = 3;
+const byte counterInterrupt = 1; 
+FreqPeriodCounter counter(counterPin, micros);
+  
+void setup(void)
+{ attachInterrupt(counterInterrupt, counterISR, CHANGE);
+}
+  
+void loop(void)
+{ int period;
+  if(counter.ready()) period = counter.period;
+}
+  
+void counterISR()
+{ counter.poll();
+}
